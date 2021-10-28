b8eb9aa4ac89cb94e51118423b2c298113824820
No life <89112548+WibuCodes@users.noreply.github.com>
2021-10-28 14:10:08 +0800

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: '🚧',
+        queue: '🎧',
+        music: '🎶',
+        success: '✅',
+    },
+
+    discord: {
+        token: 'ODc5MjUyOTU5NDAwMTgxNzcw.YSNCIQ.mn57L4aJY5ikkt6dmHMpzGH8z3k',
+        prefix: '-',
+        activity: 'Music | -help',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
