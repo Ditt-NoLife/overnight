764b7e54755cba8bd44563a295a5df4090c4cd03
No life <89112548+WibuCodes@users.noreply.github.com>
2021-10-30 21:03:13 +0800

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,6 +1,8 @@
+const Discord = require('discord.js')
+
 module.exports = {
     name: 'queue',
-    aliases: [],
+    aliases: ['q'],
     category: 'Music',
     utilisation: '{prefix}queue',
 
@@ -13,8 +15,11 @@ module.exports = {
 
         if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
 
-        message.channel.send(`**Server queue - ${message.guild.name} ${client.emotes.queue} ${client.player.getQueue(message).loopMode ? '(looped)' : ''}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`**Server queue - ${message.guild.name} ${client.emotes.queue} ${client.player.getQueue(message).loopMode ? '(looped)' : ''}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
             return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
-        }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
+        }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`))
+.setColor('#060000')
+        message.channel.send(embed)
     },
 };
