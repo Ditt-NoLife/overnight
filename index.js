5459802c1f04281b22a5dd7882ff4d41526d4d15
No life <89112548+Ditt-NoLife@users.noreply.github.com>
2021-11-20 17:40:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -116,4 +116,4 @@ client.on("clickButton", async (button) => {
 })
 //====================================================================================================================================================
 
-client.login("ODc5MjUyOTU5NDAwMTgxNzcw.YSNCIQ.ehknDWHyrZ0qXX6c_SByyD7NyIo")
+client.login()
